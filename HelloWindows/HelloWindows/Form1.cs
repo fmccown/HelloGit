a718daba9d89dfa481db837dbb5517961d27c889
Frank McCown <fmccown@harding.edu>
2021-09-21 14:01:04 -0500

    Add a new button and callback

--- HelloWindows/HelloWindows/Form1.cs
+++ HelloWindows/HelloWindows/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWindows
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "You clicked me!";
+        }
     }
 }
